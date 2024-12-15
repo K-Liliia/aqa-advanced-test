f720da3f002045af21987a846f4d04788be4a331
Liliia Kozachuk <liliiakozachuk@Liliias-MacBook-Pro-2.local>
2024-12-15 22:13:23 +0200

    DZ 4.1 JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,17 @@
+let newString;
+let newNumber;
+let newBooleanValue;
+let nullValue;
+let newUndefinedValue;
+
+newString = "This string contains some text";
+newNumber = 8;
+newBooleanValue = true;
+nullValue = null;
+
+console.log("We should see a string ---> " + newString);
+console.log("We should see a number ---> " + newNumber);
+console.log("We should see a boolean value ---> " + newBooleanValue);
+console.log("We should see a null ---> " + nullValue);
+console.log("We should see an undefined value ---> " + newUndefinedValue);
+
